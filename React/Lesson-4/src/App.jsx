7880394110049ae6b85ff6965e4ba3b2063c5394
vdeekshith2007 <vataparthideekshith18@gmail.com>
2026-07-25 23:06:18 +0530

    name changes

--- React/Lesson-4/src/App.jsx
+++ React/Lesson-4/src/App.jsx
@@ -16,11 +16,11 @@ function App(){
     <h1>Children Comp!!!!</h1>
     <Box><p>I give anything into this and this will be treated as the child of the Box class</p></Box>
 
-    <h1>For Rajesh: </h1>
+    <h1>For deekshith: </h1>
     <Student name={"Rajesh Pandit"} branch="B.Tech CSE" year={2}/>
-    <h1>For Sanjog: </h1>
+    <h1>For uday: </h1>
     <Student name={"Sanjog Gaudel"} branch="B.Tech ME" year={2}/>
-    <h1>For Suraj: </h1>
+    <h1>For raziya: </h1>
     <Student name={"Suraj Chaudhary"} branch="B.Tech ME" year={2}/>
     </div>
   );
