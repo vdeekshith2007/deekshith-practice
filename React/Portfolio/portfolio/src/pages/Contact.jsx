826ffc9c84fc05e0492db936da132b6cf483899b
vdeekshith2007 <vataparthideekshith18@gmail.com>
2026-08-01 09:46:10 +0530

    new contact form is add

--- React/Portfolio/portfolio/src/pages/Contact.jsx
+++ React/Portfolio/portfolio/src/pages/Contact.jsx
@@ -1,1 +1,33 @@
-// contact of jsx as the extended frame work of java script
+// contact of jsx as the extended frame work of java script.
+import React from "react";
+import "contact.css";
+function contact(){
+    return (
+        <div className="contact-container">
+            <p>deekshith is good </p>
+
+            <form className="contact-form">
+                <label>name</label>
+                <input type="text" placeholder="enter"
+                required
+                />
+                <label>phone</label>
+                <input type ="tel"
+                placeholder="enter"
+                />
+                <label>email message</label>
+            <textarea rows="5"
+            placeholer="enter your gmail message"
+            required
+            ></textarea>
+            <button type="submit">send your gmail</button>
+            
+            </form>
+
+        </div>
+    );
+}
+
+
+
+export default contact;
